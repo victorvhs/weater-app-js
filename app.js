46c13d25ea894363ffbb8e39529e6e59ea35bf91
victor <victor.h.s.reis@gmail.com>
2019-07-04 21:33:04 -0300

    README

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ function showError(error){
 
 function getWeather(latitude,longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
-    console.log(api)
     fetch(api)
         .then(function(response){
             let data = response.json();
@@ -61,3 +60,21 @@ function dispalyWeather(){
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city} , ${weather.country}`
 }
+
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) +32;
+}
+
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }
+    else{
+        tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
+        weather.temperature.unit = "celsius";
+    }
+})
